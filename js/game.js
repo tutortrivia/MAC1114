f0f67d7c2969e5beb00872a607623b4d426ce795
RogersMath <116944378+RogersMath@users.noreply.github.com>
2024-07-29 21:43:54 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -37,7 +37,7 @@ let sessionBest = {
     incorrect: 0,
     percentage: 0
 };
-let currentLibrary = 'StrategicPlan';
+let currentLibrary = 'JavaScript';
 let currentQuestions = [];
 let answeredQuestions = [];
 
